091537ac0eea69351f07cada8ea5eb4acf5128c7
Vitor Cavalcante <vitorcavalcante997@gmail.com>
2020-02-03 08:31:55 -0300

    finalizando projeto

---
+++ projetofinal/js/init.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAiMofQKtLHmC85KPFlcb1yKeDYvQZ4vWE",
+    authDomain: "projeto-final-dd354.firebaseapp.com",
+    databaseURL: "https://projeto-final-dd354.firebaseio.com",
+    projectId: "projeto-final-dd354",
+    storageBucket: "projeto-final-dd354.appspot.com",
+    messagingSenderId: "112485214227",
+    appId: "1:112485214227:web:3368959d2fcf22686bf7e6",
+    measurementId: "G-Q1Y8MLSXRV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
